ca9035a86ccdd9ca0c97d98c395aecce2746618a
juserh <rosapark07@gmail.com>
2024-12-03 18:35:10 +0900

    1단계

---
+++ Step1.py
@@ -0,0 +1,66 @@
+#Q2557
+print("Hello World!")
+
+#Q1000
+a, b = map(int, input().split())
+print(a+b)
+
+#Q1001
+a, b = map(int, input().split())
+print(a-b)
+
+#Q10998
+a, b = map(int, input().split())
+print(a*b)
+
+#Q1008
+a, b = map(int, input().split())
+print(a/b)
+
+#Q10869
+a, b= map(int, input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+
+#Q10926
+a = input()
+print(a+"??!")
+
+#Q18108
+a = int(input())
+print(a-543)
+
+#Q10430
+a, b, c = map(int, input().split())
+print((a+b)%c)
+print(((a%c)+(b%c))%c)
+print((a*b)%c)
+print(((a%c)*(b%c))%c)
+
+#Q2588
+a = int(input())
+b = int(input())
+print(a*(b%10))
+print(a*(b//10%10))
+print(a*(b//100))
+print(a*b)
+
+#Q11382
+a, b, c = map(int, input().split())
+print(a+b+c)
+
+#Q10171
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \\(__)|")
+
+#Q10172
+print("|\_/|")
+print("|q p|   /}")
+print("( 0 )\"\"\"\\")
+print("|\"^\"`    |")
+print("||_/=\\\\__|")
