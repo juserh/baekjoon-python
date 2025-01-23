6e7dfbfede94e3683ff309446b8b0ce3e45c9fc0
juserh <rosapark07@gmail.com>
2025-01-23 10:28:55 +0900

    12단계: Q2231

--- Step12.py
+++ Step12.py
@@ -1,15 +1,27 @@
-#Q2798
-n, m = map(int, input().split())
-arr = list(map(int, input().split()))
-arr.sort(reverse=True)
-
-total_m = 0
-for i in range(n-2):
-    for j in range(i+1,n-1):
-        for k in range(j+1, n):
-            total = arr[i] + arr[j] + arr[k]
-            if (total<=m) and (total>total_m):
-                total_m = total
-print(total_m)
+# #Q2798
+# n, m = map(int, input().split())
+# arr = list(map(int, input().split()))
+# arr.sort(reverse=True)
+#
+# total_m = 0
+# for i in range(n-2):
+#     for j in range(i+1,n-1):
+#         for k in range(j+1, n):
+#             total = arr[i] + arr[j] + arr[k]
+#             if (total<=m) and (total>total_m):
+#                 total_m = total
+# print(total_m)
 
 #Q2231
+n = int(input())
+
+for i in range(1,n+1):
+    num = sum((map(int, str(i))))
+    num_sum = i+num
+    if num_sum == n:
+        print(i)
+        break
+    if i ==n :
+        print(0)
+
+#Q19532
