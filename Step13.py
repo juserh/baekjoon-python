8bf5380c0e5d382fada9704318d17a4b1f4ff85f
juserh <rosapark07@gmail.com>
2025-01-31 09:52:24 +0900

    13단계: Q2587

--- Step13.py
+++ Step13.py
@@ -1,20 +1,28 @@
 # #Q2750
-n = int(input())
-arr = []
-for _ in range(n) :
-    arr.append(int(input()))
-##(1)
-# arr.sort()
+# n = int(input())
+# arr = []
+# for _ in range(n) :
+#     arr.append(int(input()))
+# #(1)
+# # arr.sort()
+# # for a in arr :
+# #     print(a)
+#
+# #(2) -버블정렬
+# for i in range(n-1,-1,-1) :
+#     for j in range(0, i) :
+#         if arr[j] > arr[j+1] :
+#             tmp = arr[j+1]
+#             arr[j+1] = arr[j]
+#             arr[j] = tmp
+#
 # for a in arr :
 #     print(a)
 
-#(2) -버블정렬
-for i in range(n-1,-1,-1) :
-    for j in range(0, i) :
-        if arr[j] > arr[j+1] :
-            tmp = arr[j+1]
-            arr[j+1] = arr[j]
-            arr[j] = tmp
+#Q2587
+arr = []
+for _ in range(5) :
+    arr.append(int(input()))
 
-for a in arr :
-    print(a)
+print(int(sum(arr)/5))
+print(sorted(arr)[2])
