2e03c36496be02788eb4181e0d31b41ff7b68340
juserh <rosapark07@gmail.com>
2025-02-11 22:21:22 +0900

    13단계: Q11651

--- Step13.py
+++ Step13.py
@@ -58,11 +58,21 @@
 # for a in sorted(n, reverse=True) :
 #     print(a,end="")
 
-#Q11650
+# #Q11650
+# n = int(input())
+# arr = []
+# for _ in range(n) :
+#     x, y = map(int, input().split())
+#     arr.append([x,y])
+# for a in sorted(arr) : #or arr.sort()
+#     print(a[0],a[1])
+
+#Q11651
 n = int(input())
 arr = []
 for _ in range(n) :
     x, y = map(int, input().split())
     arr.append([x,y])
-for a in sorted(arr) : #or arr.sort()
+arr.sort(key = lambda x : (x[1], x[0]))
+for a in arr :
     print(a[0],a[1])
