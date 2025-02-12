085d000e0c290d6837701e87c72e9b68fc10650f
juserh <rosapark07@gmail.com>
2025-02-12 17:03:04 +0900

    13단계: Q1181

--- Step13.py
+++ Step13.py
@@ -67,12 +67,22 @@
 # for a in sorted(arr) : #or arr.sort()
 #     print(a[0],a[1])
 
-#Q11651
+# #Q11651
+# n = int(input())
+# arr = []
+# for _ in range(n) :
+#     x, y = map(int, input().split())
+#     arr.append([x,y])
+# arr.sort(key = lambda x : (x[1], x[0]))
+# for a in arr :
+#     print(a[0],a[1])
+
+#Q1181
 n = int(input())
 arr = []
 for _ in range(n) :
-    x, y = map(int, input().split())
-    arr.append([x,y])
-arr.sort(key = lambda x : (x[1], x[0]))
-for a in arr :
-    print(a[0],a[1])
+    arr.append(input()) #arr.append(sys.stdin.readline().strip())
+arr = list(set(arr))
+arr.sort(key = lambda x : (len(x), x))
+for w in arr :
+    print(w)
