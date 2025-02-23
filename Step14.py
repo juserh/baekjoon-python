26e3de8d260094e843c1fc2cb69e6a37d582a4d9
juserh <rosapark07@gmail.com>
2025-02-23 16:47:00 +0900

    14단계: Q14425

--- Step14.py
+++ Step14.py
@@ -1,13 +1,25 @@
-#Q10815
-import sys
-n = int(input())
-arr = list(map(int, sys.stdin.readline().split()))
-m = int(input())
-nums = list(map(int, sys.stdin.readline().split()))
-dic = {n: 0 for n in nums}
-for a in arr :
-    if a in dic :
-        dic[a] = 1
+# #Q10815
+# import sys
+# n = int(input())
+# arr = list(map(int, sys.stdin.readline().split()))
+# m = int(input())
+# nums = list(map(int, sys.stdin.readline().split()))
+# dic = {n: 0 for n in nums}
+# for a in arr :
+#     if a in dic :
+#         dic[a] = 1
+#
+# for d in dic :
+#     print(dic[d], end=" ")
 
-for d in dic :
-    print(dic[d], end=" ")
+#Q14425
+n, m = map(int, input().split())
+s = set()
+for _ in range(n) :
+    s.add(input())
+cnt = 0
+for _ in range(m) :
+    i = input()
+    if i in s :
+        cnt += 1
+print(cnt)
