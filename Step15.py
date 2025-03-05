2391c473806ad665a3628ff1df22d9116ea82e0d
juserh <rosapark07@gmail.com>
2025-03-05 09:38:12 +0900

    15단계: Q13241

--- Step15.py
+++ Step15.py
@@ -1,16 +1,23 @@
-#Q1934
-t = int(input())
-for _ in range(t) :
-    a, b = map(int, input().split())
+# #Q1934
+# t = int(input())
+# for _ in range(t) :
+#     a, b = map(int, input().split())
+#
+#     result = a*b
+#     while b>0 :
+#         a, b = b, a%b
+#     print(result // a)
+# """
+# #내장 라이브러리
+# import math
+# for _ in range(t) :
+#     a,b = map(int, input().split())
+#     print(math.lcm(a,b))
+# """
 
-    result = a*b
-    while b>0 :
-        a, b = b, a%b
-    print(result // a)
-"""
-#내장 라이브러리
-import math
-for _ in range(t) :
-    a,b = map(int, input().split())
-    print(math.lcm(a,b))
-"""
+#Q13241
+a, b = map(int, input().split())
+result = a*b
+while b > 0 :
+    a, b = b, a%b
+print(result//a)
