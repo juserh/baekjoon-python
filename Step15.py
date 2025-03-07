e0acd273db37d437aac8280738b99ccb67ddc3b7
juserh <rosapark07@gmail.com>
2025-03-07 18:02:26 +0900

    15단계: Q2485

--- Step15.py
+++ Step15.py
@@ -22,14 +22,32 @@
 #     a, b = b, a%b
 # print(result//a)
 
-#Q1735
-def gcd(x1, x2) :
-    while x2 > 0 :
-        x1, x2 = x2, x1 % x2
-    return x1
-a1, a2 = map(int, input().split())
-b1, b2 = map(int, input().split())
-s1 = b2 * a1 + a2 * b1
-s2 = a2 * b2
-n = gcd(s1, s2)
-print(s1//n, s2//n)
+# #Q1735
+# def gcd(x1, x2) :
+#     while x2 > 0 :
+#         x1, x2 = x2, x1 % x2
+#     return x1
+# a1, a2 = map(int, input().split())
+# b1, b2 = map(int, input().split())
+# s1 = b2 * a1 + a2 * b1
+# s2 = a2 * b2
+# n = gcd(s1, s2)
+# print(s1//n, s2//n)
+
+#Q2485
+def gcd_count(li) :
+    a = li[0]
+    for i in range(1,n-1) :
+        b = li[i]
+        while b > 0 :
+            a, b = b, a%b
+    sum = 0
+    for l in li :
+        sum += (l//a-1)
+    return sum
+
+n = int(input())
+
+arr = [int(input()) for _ in range(n)]
+dis = [(arr[i]-arr[i-1]) for i in range(1,n)]
+print(gcd_count(dis))
