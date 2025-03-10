94fb4fab8037b179a5b3ab2c4770772aa5258768
juserh <rosapark07@gmail.com>
2025-03-10 10:59:03 +0900

    15단계: Q4134

--- Step15.py
+++ Step15.py
@@ -34,20 +34,44 @@
 # n = gcd(s1, s2)
 # print(s1//n, s2//n)
 
-#Q2485
-def gcd_count(li) :
-    a = li[0]
-    for i in range(1,n-1) :
-        b = li[i]
-        while b > 0 :
-            a, b = b, a%b
-    sum = 0
-    for l in li :
-        sum += (l//a-1)
-    return sum
+# #Q2485
+# def gcd_count(li) :
+#     a = li[0]
+#     for i in range(1,n-1) :
+#         b = li[i]
+#         while b > 0 :
+#             a, b = b, a%b
+#     sum = 0
+#     for l in li :
+#         sum += (l//a-1)
+#     return sum
+#
+# n = int(input())
+#
+# arr = [int(input()) for _ in range(n)]
+# dis = [(arr[i]-arr[i-1]) for i in range(1,n)]
+# print(gcd_count(dis))
 
-n = int(input())
+#Q4134
+import sys
+def is_decimal(num) :
+    for i in range(2, int(num**0.5)+1) :
+        if num%i == 0 :
+            return False
+    return True
 
-arr = [int(input()) for _ in range(n)]
-dis = [(arr[i]-arr[i-1]) for i in range(1,n)]
-print(gcd_count(dis))
+n = int(sys.stdin.readline())
+for _ in range(n) :
+    a = int(sys.stdin.readline())
+    r = False
+    while not r:
+        if a==0 or a==1 :
+            print(2)
+            break
+        r = is_decimal(a)
+        if not r:
+            a += 1
+        else :
+            print(a)
+            break
+
