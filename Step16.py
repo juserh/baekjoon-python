5175e09a410610e208fc9a69330d9f3d297fdc7f
juserh <rosapark07@gmail.com>
2025-04-02 09:39:41 +0900

    16단계: Q4949

--- Step16.py
+++ Step16.py
@@ -39,26 +39,55 @@
 #         stack.append(n)
 # print(sum(stack))
 
-#Q9012
+# #Q9012
+# import sys
+#
+# t = int(sys.stdin.readline())
+#
+# for _ in range(t) :
+#     s = sys.stdin.readline().strip()
+#     stack = []
+#     for c in s :
+#         if c==')' :
+#             if len(stack) == 0 :
+#                 stack.append(c)
+#                 break
+#             elif stack[-1] == '(':
+#                 stack.pop()
+#             else :
+#                 stack.append(c)
+#         else :
+#             stack.append(c)
+#     if len(stack) == 0:
+#         print('YES')
+#     else :
+#         print('NO')
+
+#Q4949
 import sys
 
-t = int(sys.stdin.readline())
-
-for _ in range(t) :
-    s = sys.stdin.readline().strip()
+while True :
+    s = sys.stdin.readline().rstrip()
+    if s == "." : break
     stack = []
     for c in s :
-        if c==')' :
-            if len(stack) == 0 :
-                stack.append(c)
-                break
-            elif stack[-1] == '(':
+        if c == '[' or c =='(' :
+            stack.append(c)
+        elif c == ']' :
+            if len(stack) != 0 and stack[-1] == '[' :
                 stack.pop()
             else :
                 stack.append(c)
-        else :
-            stack.append(c)
-    if len(stack) == 0:
-        print('YES')
-    else :
-        print('NO')
+                break
+        elif c == ')' :
+            if len(stack) != 0 and stack[-1] =='(' :
+                stack.pop()
+            else :
+                stack.append(c)
+                break
+    if len(stack) == 0 :
+        print('yes')
+    else:
+        print('no')
+
+
