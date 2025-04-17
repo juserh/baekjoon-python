408e31ccbfebdf6257feec5555cb0fd604ecd589
juserh <rosapark07@gmail.com>
2025-04-17 10:40:14 +0900

    17단계: Q10872

--- Step17.py
+++ Step17.py
@@ -3,7 +3,16 @@
 # n = int(sys.stdin.readline())
 # print(n*(n-1))
 
-#Q24723
+# #Q24723
+# import sys
+# n = int(sys.stdin.readline())
+# print(2**n)
+
+#Q10872
 import sys
 n = int(sys.stdin.readline())
-print(2**n)
+result = 1
+
+for i in range(2, n+1) :
+    result *= i
+print(result)
