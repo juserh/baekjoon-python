ef8bc599a33720ccb42a66db2522c4f6f8e4b8fb
juserh <rosapark07@gmail.com>
2025-04-23 10:31:20 +0900

    18단계: Q25192

--- Step18.py
+++ Step18.py
@@ -1,8 +1,26 @@
-#Q1037
+# #Q1037
+# import sys
+#
+# n = int(sys.stdin.readline())
+# arr = list(map(int, sys.stdin.readline().split()))
+# mx = max(arr)
+# mn = min(arr)
+# print(mx*mn)
+
+#Q25192
 import sys
 
 n = int(sys.stdin.readline())
-arr = list(map(int, sys.stdin.readline().split()))
-mx = max(arr)
-mn = min(arr)
-print(mx*mn)
+emoji = set()
+
+cnt = 0
+for _ in range(n) :
+    chat = sys.stdin.readline().rstrip()
+
+    if chat == "ENTER" :
+        emoji.clear()
+    else :
+        if chat not in emoji :
+            cnt += 1
+            emoji.add(chat)
+print(cnt)
