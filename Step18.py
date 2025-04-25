c7949f675a13a7442ccbcd80333fd0ad4d39639f
juserh <rosapark07@gmail.com>
2025-04-25 17:18:35 +0900

    18단계: Q2108

--- Step18.py
+++ Step18.py
@@ -25,17 +25,49 @@
 # cnt += len(emoji)
 # print(cnt)
 
-#Q26069
+# #Q26069
+# import sys
+#
+# n = int(sys.stdin.readline())
+# dance = set(["ChongChong"])
+#
+# for _ in range(n) :
+#     p1 , p2 = sys.stdin.readline().split()
+#
+#     if p1 in dance:
+#         dance.add(p2)
+#     elif p2 in dance :
+#         dance.add(p1)
+# print(len(dance))
+
+#Q2108
 import sys
 
 n = int(sys.stdin.readline())
-dance = set(["ChongChong"])
+arr = [int(sys.stdin.readline()) for _ in range(n)]
+arr = sorted(arr)
 
-for _ in range(n) :
-    p1 , p2 = sys.stdin.readline().split()
 
-    if p1 in dance:
-        dance.add(p2)
-    elif p2 in dance :
-        dance.add(p1)
-print(len(dance))
+#산술평균
+print(round(sum(arr)/n))
+#중앙값
+print(arr[n//2])
+#최빈값
+dic = dict()
+for a in arr :
+    if a in dic :
+        dic[a] += 1
+    else :
+        dic[a] = 1
+mx = max(dic.values())
+oft = []
+
+for a in dic :
+    if mx == dic[a] :
+        oft.append(a)
+if len(oft) == 1:
+    print(oft[0])
+else :
+    print(oft[1])
+#범위
+print(max(arr) - min(arr))
