d58e24a33770fa7a6f47c839ff12db08b4293443
juserh <rosapark07@gmail.com>
2025-04-30 11:18:42 +0900

    19단계: Q10870

--- Step19.py
+++ Step19.py
@@ -1,9 +1,19 @@
-#Q27433
+# #Q27433
+# import sys
+# def factorial(num) :
+#     if num <=1 :
+#         return 1
+#     return factorial(num-1)*num
+#
+# n = int(sys.stdin.readline())
+# print(factorial(n))
+
+#Q10870
 import sys
-def factorial(num) :
-    if num <=1 :
-        return 1
-    return factorial(num-1)*num
+def fibo(num) :
+    if num <= 1 :
+        return num
+    return fibo(num-1) + fibo(num-2)
 
 n = int(sys.stdin.readline())
-print(factorial(n))
+print(fibo(n))
