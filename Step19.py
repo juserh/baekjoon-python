34632be7eb8b883dfa4f644801a41988929bb893
juserh <rosapark07@gmail.com>
2025-05-01 13:43:21 +0900

    19단계: Q25501

--- Step19.py
+++ Step19.py
@@ -8,12 +8,35 @@
 # n = int(sys.stdin.readline())
 # print(factorial(n))
 
-#Q10870
-import sys
-def fibo(num) :
-    if num <= 1 :
-        return num
-    return fibo(num-1) + fibo(num-2)
+# #Q10870
+# import sys
+# def fibo(num) :
+#     if num <= 1 :
+#         return num
+#     return fibo(num-1) + fibo(num-2)
+#
+# n = int(sys.stdin.readline())
+# print(fibo(n))
 
-n = int(sys.stdin.readline())
-print(fibo(n))
+#Q25501
+import sys
+
+def recursion(st, l, r) :
+    global cnt #전역변수
+    cnt += 1
+    if l >= r :
+        return [1, cnt]
+    elif s[l] != s[r] :
+        return [0, cnt]
+    else :
+        return recursion(st, l+1, r-1)
+
+def isPalindrome(st) :
+    return recursion(st, 0, len(st)-1)
+
+t = int(sys.stdin.readline())
+
+for _ in range(t) :
+    cnt = 0
+    s = sys.stdin.readline().rstrip()
+    print(*isPalindrome(s))
