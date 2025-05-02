0e09c61b8e82cc39c34445306aea2e1bd325734a
juserh <rosapark07@gmail.com>
2025-05-02 14:52:18 +0900

    19단계: 24060

--- Step19.py
+++ Step19.py
@@ -18,25 +18,71 @@
 # n = int(sys.stdin.readline())
 # print(fibo(n))
 
-#Q25501
+# #Q25501
+# import sys
+#
+# def recursion(st, l, r) :
+#     global cnt #전역변수
+#     cnt += 1
+#     if l >= r :
+#         return [1, cnt]
+#     elif s[l] != s[r] :
+#         return [0, cnt]
+#     else :
+#         return recursion(st, l+1, r-1)
+#
+# def isPalindrome(st) :
+#     return recursion(st, 0, len(st)-1)
+#
+# t = int(sys.stdin.readline())
+#
+# for _ in range(t) :
+#     cnt = 0
+#     s = sys.stdin.readline().rstrip()
+#     print(*isPalindrome(s))
+
+#Q24060
 import sys
 
-def recursion(st, l, r) :
-    global cnt #전역변수
-    cnt += 1
-    if l >= r :
-        return [1, cnt]
-    elif s[l] != s[r] :
-        return [0, cnt]
-    else :
-        return recursion(st, l+1, r-1)
+def merge_sort(li) :
+    global saved
 
-def isPalindrome(st) :
-    return recursion(st, 0, len(st)-1)
+    if len(li) == 1 :
+        return li
 
-t = int(sys.stdin.readline())
+    mid = (len(li)+1) // 2
 
-for _ in range(t) :
-    cnt = 0
-    s = sys.stdin.readline().rstrip()
-    print(*isPalindrome(s))
+    left = merge_sort(li[:mid])
+    right = merge_sort(li[mid:])
+
+    i, j = 0, 0
+
+    tmp = []
+    while (i< len(left) and j< len(right)) :
+        if left[i] <= right[j] :
+            tmp.append(left[i])
+            i += 1
+        else :
+            tmp.append(right[j])
+            j += 1
+
+    while i<len(left) :
+        tmp.append(left[i])
+        i += 1
+    while j<len(right):
+        tmp.append(right[j])
+        j += 1
+
+    for t in tmp :
+        saved.append(t)
+    return tmp
+
+n, k = map(int, sys.stdin.readline().split())
+a = list(map(int, sys.stdin.readline().split()))
+
+saved = []
+merge_sort(a)
+if len(saved) < k :
+    print(-1)
+else :
+ print(saved[k-1])
