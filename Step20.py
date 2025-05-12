c96affc11df2464650f0e722f8a560fc58d6e116
juserh <rosapark07@gmail.com>
2025-05-12 11:20:59 +0900

    20단계: Q15651

--- Step20.py
+++ Step20.py
@@ -16,25 +16,42 @@
 # n, m = map(int, sys.stdin.readline().split())
 # backtracking([], n)
 
-#Q15650
+# #Q15650
+# import sys
+#
+# def backtracking(li, num) :
+#     global m
+#     global visited
+#     if len(li) == m :
+#         print(' '.join(map(str, li)))
+#
+#     for i in range(1, num+1) :
+#         if visited[i-1] == 0 :
+#             if len(li) == 0 or li[-1] < i :
+#                 li.append(i)
+#                 visited[i-1] = 1
+#                 backtracking(li, num)
+#                 li.pop()
+#                 visited[i-1] = 0
+#
+#
+# n, m = map(int, sys.stdin.readline().split())
+# visited = [0] * n
+# backtracking([], n)
+
+#Q15651
 import sys
 
 def backtracking(li, num) :
     global m
-    global visited
     if len(li) == m :
         print(' '.join(map(str, li)))
+        return
 
     for i in range(1, num+1) :
-        if visited[i-1] == 0 :
-            if len(li) == 0 or li[-1] < i :
-                li.append(i)
-                visited[i-1] = 1
-                backtracking(li, num)
-                li.pop()
-                visited[i-1] = 0
-
+        li.append(i)
+        backtracking(li, num)
+        li.pop()
 
 n, m = map(int, sys.stdin.readline().split())
-visited = [0] * n
 backtracking([], n)
