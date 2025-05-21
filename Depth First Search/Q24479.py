f47756d68a8dbf37bc1578453740fe2d7e5ddf8d
juserh <rosapark07@gmail.com>
2025-05-21 14:10:39 +0900

    dfs : Q24479 추가

--- Depth First Search/Q24479.py
+++ Depth First Search/Q24479.py
@@ -1,4 +1,5 @@
 import sys
+"""
 sys.setrecursionlimit(10**6)
 
 def dfs(r) :
@@ -13,10 +14,34 @@ def dfs(r) :
         for a in arr :
             if visited[a] == 0 :
                 dfs(a)
+"""
+
+def dfs_stack(r) :
+    global dic
+    visited = [0]*(n+1)
+    cnt = 0
+    stack = [r]
+
+    while stack :
+        current = stack.pop(-1)
+
+        if visited[current] != 0 :
+            continue
+
+        cnt += 1
+        visited[current] = cnt
+
+        if current in dic :
+            arr = sorted(dic[current], reverse = True)
+            for a in arr :
+                if visited[a] == 0 :
+                    stack.append(a)
+    return visited
 
 n, m, r = map(int, sys.stdin.readline().split())
 dic = {}
-visited = [0]*(n+1)
+# visited = [0]*(n+1) #-dfs
+# cnt = 0 #-dfs
 
 for _ in range(m) :
     u, v = map(int, sys.stdin.readline().split())
@@ -29,7 +54,8 @@ for _ in range(m) :
     if v not in dic :
         dic[v] = [u]
 
-cnt = 0
-dfs(r)
+#dfs(r)
+
+visited = dfs_stack(r)
 for i in range(1, n+1) :
     print(visited[i])
