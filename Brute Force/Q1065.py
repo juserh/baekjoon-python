14e3de3a61985b705628c2ce66f25318494ad613
juserh <rosapark07@gmail.com>
2025-06-02 11:07:09 +0900

    brute force: Q1065 코드 추가

--- Brute Force/Q1065.py
+++ Brute Force/Q1065.py
@@ -16,3 +16,23 @@ else :
         else :
             cnt += 1
 print(cnt)
+
+"""
+#문제 조건 n<=1000을 반영한 풀이
+import sys
+
+n = int(sys.stdin.readline())
+
+cnt = 0
+if n < 100 :
+    cnt = n
+else :
+    cnt += 99
+    for num in range(100, n+1) :
+        s = list(map(int, str(num)))
+        d1 = s[1] - s[0]
+        d2 = s[2] - s[1]
+        if d2 == d1 :
+            cnt += 1
+print(cnt)
+"""
