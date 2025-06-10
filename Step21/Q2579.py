6a5dc55cf6b2f3cf768ef4f9955137ef68090292
juserh <rosapark07@gmail.com>
2025-06-10 11:48:28 +0900

    21단계: Q2579(2)

--- Step21/Q2579.py
+++ Step21/Q2579.py
@@ -1,15 +1,15 @@
 import sys
 
 n = int(sys.stdin.readline())
-arr = [0]
-for _ in range(n) :
-    arr.append(int(sys.stdin.readline()))
+arr = [0]*301
+for i in range(1, n+1) :
+    arr[i] = int(sys.stdin.readline())
 
-dp = [0]*(n+1)
-if len(arr) <= 2 :
-    print(sum(arr))
-else :
-    dp = [0, arr[1], arr[1]+arr[2]]
-    for i in range(3, n+1) :
-        dp.append(max(arr[i]+arr[i-1]+dp[i-3], arr[i]+dp[i-2]))
-    print(dp[n])
+dp = [0]*301
+
+dp[1] = arr[1]
+dp[2] = arr[1]+arr[2]
+
+for i in range(3, n+1) :
+    dp[i] = max(arr[i]+arr[i-1]+dp[i-3], arr[i]+dp[i-2])
+print(dp[n])
